f71bd3ce988646f1de5b66e37e4493e921ad188c
Andrew Winter <ajw278@ast.cam.ac.uk>
2016-12-20 17:33:37 +0000

    initial setup, some potentially useful functions defined

---
+++ pokerpy.py
@@ -0,0 +1,14 @@
+"""=================================================================================================
+//  POKERPY :
+//  Python code for automated poker learning and playing.
+//  https://github.com/ajw278/pokerpy.git
+//  Contact : ajwinter@ast.cam.ac.uk
+//  Contributors : Andrew Winter, Pablo Lemos Portela, Cameron Lemon
+//  Description : 
+//
+//  Last edited: 20/12/2016
+//================================================================================================="""
+
+
+#pokerpy.py 
+#Main function: 
